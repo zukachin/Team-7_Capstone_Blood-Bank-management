ab586adfcbbe4b47a65e2c4ce320cc4c0caeb039
adarsh <sinhaadarsh929@gmail.com>
2025-09-04 15:51:38 +0530

    update pool db

--- donor-service/src/db/pool.js
+++ donor-service/src/db/pool.js
@@ -1,11 +1,11 @@
 const pg = require("pg");
 
 const pool = new pg.Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "susi2003",
-  database: "blood_bank"
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
 });
 
 module.exports = { pool };
